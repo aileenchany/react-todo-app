ab45b2e3952add90c05f29c25eacdc33b47c78e4
Aileen CM <aileenchany@gmail.com>
2022-11-17 19:55:15 -0500

    Added assets folder and initial loginForm validation

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,25 +1,59 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const LoginForm = () => {
+  /* testEmail & testPassword are for testing purposes only */
+  const testEmail = 'test@rapptrlabs.com';
+  const testPassword = 'Test123';
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const validateInputs = (e) => {
+    e.preventDefault();
+
+    if (email === testEmail && password === testPassword) {
+      navigate('/list');
+    } else if (email !== testEmail || password !== testPassword) {
+      const hidden = document.getElementById('hidden');
+      hidden.textContent = 'Incorrect email or password';
+    }
+  };
+
   return (
-    <form className='LoginForm'>
-      <label for='email'>Email</label>
+    <form className='LoginForm' onSubmit={validateInputs}>
+      <label htmlFor='email'>Email</label>
       <input
         type='email'
         id='email'
         name='email'
-        z
+        aria-label='Email field'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         placeholder='user@rapptrlabs.com'
+        autoComplete=''
         required
       />
-      <label for='password'>Password</label>
+      <label htmlFor='password'>Password</label>
       <input
         type='password'
         id='password'
         name='password'
+        aria-label='Password field'
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+          console.log(password);
+        }}
         placeholder='Must be at least 4 characters'
-        maxLength={4}
+        autoComplete=''
+        minLength={4}
         required
       />
-      <button type='submit'>Login</button>
+      <small id='hidden'></small>
+      <button type='submit' disabled={email === '' || password === ''}>
+        Login
+      </button>
     </form>
   );
 };
